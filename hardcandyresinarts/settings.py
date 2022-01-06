e74dbc06b07e717f1b707f21bf35d2f1d140064b
Aramantos <john.doyle.mail@icloud.com>
2022-01-06 17:51:38 +0000

    update secret key

--- hardcandyresinarts/settings.py
+++ hardcandyresinarts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-71s+-azo)vxgdfciv!(_il1n#=bc(n6l35%9ho&v57+^y!dw+*'
+# SECRET KEY
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
