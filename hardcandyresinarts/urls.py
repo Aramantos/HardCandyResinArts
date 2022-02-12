dc798f9899417a5746e72ff9a7c604c0f7236c3b
Aramantos <john.doyle.mail@icloud.com>
2022-02-12 21:13:21 +0000

    add basket template, urls & views

--- hardcandyresinarts/urls.py
+++ hardcandyresinarts/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('shop/', include('shop.urls')),
     path('about/', include('about.urls')),
+    path('basket/', include('basket.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
