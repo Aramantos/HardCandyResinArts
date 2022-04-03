c97a0783ab0a6e2e4a886dcee8ff57275750a6c7
Aramantos <john.doyle.mail@icloud.com>
2022-04-03 19:03:33 +0000

    update database urls & prep for first deploy

--- hardcandyresinarts/settings.py
+++ hardcandyresinarts/settings.py
@@ -16,6 +16,7 @@ import os
 if os.path.exists("env.py"):
     import env
 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,12 +129,18 @@ WSGI_APPLICATION = 'hardcandyresinarts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASE
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
